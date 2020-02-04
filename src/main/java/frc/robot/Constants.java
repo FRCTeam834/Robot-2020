0f5cf3521b5469356c8976a29e64c62d3d420431
mdurrani808 <46766905+mdurrani808@users.noreply.github.com>
2020-02-03 20:21:03 -0500

    update constants.java
    oops i did it again

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -61,5 +61,9 @@ public final class Constants {
     public static final double kTrackwidthMeters = 0;
     public static final DifferentialDriveKinematics kDriveKinematics =
         new DifferentialDriveKinematics(kTrackwidthMeters);
+    public static final double kMaxSpeedMetersPerSecond = 0;
+    public static final double kMaxAccelerationMetersPerSecondSquared = 0;
+    public static final double kRamseteB = 2;
+    public static final double kRamseteZeta = 0.7;
 
 }
